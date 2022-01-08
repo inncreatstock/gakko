648038d63892015ac9fd68131f9be1d54fd5ce9c
inncreatstock <89166958+inncreatstock@users.noreply.github.com>
2022-01-08 12:07:41 -0600

    Add files via upload

---
+++ js/script_section.js
@@ -0,0 +1,90 @@
+//==========__________RELATED-IMAGE__________==========
+
+var link1
+
+	link1 = document.getElementById("link1");
+	link1.href="https://www.inncreat-gallery.ga/photography/photographs/32/photography";
+
+var related1
+
+	related1 = document.getElementById("related1");
+	related1.src="https://www.mediafire.com/convkey/8321/ikg8xzg79d143a97g.jpg";
+
+var link2
+
+	link2 = document.getElementById("link2");
+	link2.href="https://www.inncreat-gallery.ga/photography/photographs/26/photography";
+
+var related2
+
+	related2 = document.getElementById("related2");
+	related2.src="https://www.mediafire.com/convkey/5d6f/3be3vhdui2uiqmt7g.jpg";
+
+var link3
+
+	link3 = document.getElementById("link3");
+	link3.href="https://www.inncreat-gallery.ga/photography/photographs/33/photography";
+
+var related3
+
+	related3 = document.getElementById("related3");
+	related3.src="https://www.mediafire.com/convkey/6657/ih3zci98b22gv847g.jpg";
+
+var link4
+
+	link4 = document.getElementById("link4");
+	link4.href="https://www.inncreat-gallery.ga/photography/photographs/1/photography";
+
+var related4
+
+	related4 = document.getElementById("related4");
+	related4.src="https://www.mediafire.com/convkey/30e7/u4hmna3bfsjkuld7g.jpg";
+
+//==========__________RELATED__________==========
+
+var related;
+
+	related = document.getElementById("related");
+	related.innerHTML = "Mas populares:";
+
+//==========__________DESCRIPTION__________==========
+
+var description;
+
+	description = document.getElementById("description");
+	description.innerHTML = "Descripci&oacute;n:";
+
+//==========__________QUESTIONS__________==========
+
+var question1;
+
+	question1 = document.getElementById("question1");
+	question1.innerHTML = "Porque las im&aacute;genes dentro de la p&aacute;gina son de menor calidad?";
+
+var answer1;
+
+	answer1 = document.getElementById("answer1");
+	answer1.innerHTML = "Ya que no contamos con nuestros propios servidores para subir las im&aacute;genes debemos de cuidar el espacio que se nos presta y por lo tanto antes de subir las im&aacute;genes se les baja la calidad y se comprimen y as&iacute; logramos que pesen 5 veces menos su peso original o hasta m&aacute;s, pero no te preocupes! al descargarla se descargara en su tama&ntilde;o original.";
+
+var question2;
+
+	question2 = document.getElementById("question2");
+	question2.innerHTML = "Cu&aacute;ndo contaran con sus propios servidores?";
+
+var answer2;
+
+	answer2 = document.getElementById("answer2");
+	answer2.innerHTML = "La verdad para nuestros propios servidores falta mucho tiempo, pero lo que si le podemos asegurar es que muy pronto contaremos con nuestra propia direcci&oacute;n &#34;.com&#34;, Cuando? Pues en cuanto obtengamos nuestra primera paga por base a los anuncios de esta p&aacute;gina.";
+
+var question3;
+
+	question3 = document.getElementById("question3");
+	question3.innerHTML = "Como puedo anunciarme en esta p&aacute;gina?";
+
+var answer3;
+
+	answer3 = document.getElementById("answer3");
+	answer3.innerHTML = "Es muy simple! lo bueno de no recurrir a google ads es que puedes anunciar lo que quieras en esta p&aacute;gina, desde un canal de YouTube hasta un restaurante, para hacerlo simplemente debes de dar click en la cabecera de la p&aacute;gina o en el anuncio sobre el bot&oacute;n de descarga cuando no tengan un anuncio o dar click en el pie de p&aacute;gina en el apartado &#34;an&uacute;nciate&#34;.";
+
+
+
